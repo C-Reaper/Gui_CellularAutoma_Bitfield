eee048710bfe8d206c6899bf5ae505735f627767
codeleaded <gnu.c.reaper@gmail.com>
2026-05-11 22:07:59 +0200

    init commit

---
+++ src/Main.c
@@ -0,0 +1,125 @@
+// #include "C:/Wichtig/System/Static/Library/WindowEngine1.0.h"
+// #include "C:/Wichtig/System/Static/Library/Victor2.h"
+// #include "C:/Wichtig/System/Static/Library/Vdctor2.h"
+// #include "C:/Wichtig/System/Static/Library/Complex.h"
+// #include "C:/Wichtig/System/Static/Library/TransformedView.h"
+// #include "C:\Wichtig\System\Static\Container\Vector.h"
+
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/TransformedView.h"
+#include "/home/codeleaded/System/Static/Container/Bitfield.h"
+
+
+#define WORLD_X			100
+#define WORLD_Y			100
+
+#define WORLD_ERROR 	-1
+#define WORLD_INACTIVE	0
+#define WORLD_ACTIVE	1
+
+
+Bitfield world;
+Bitfield world_target;
+TransformedView tv;
+
+char World_Get(Bitfield* world,int x,int y){
+	if(x<0 || x>=WORLD_X || y<0 || y>=WORLD_Y) return WORLD_ERROR;
+	return Bitfield_Get(world,y * WORLD_X + x);
+}
+void World_Set(Bitfield* world,int x,int y,char c){
+	if(x<0 || x>=WORLD_X || y<0 || y>=WORLD_Y) return;
+	Bitfield_Set(world,y * WORLD_X + x,c);
+}
+char World_Get_N(Bitfield* world,int x,int y){
+	if(x<0 || x>=WORLD_X || y<0 || y>=WORLD_Y) return 0;
+	
+	char count = 0;
+	for(int i = -1;i<=1;i++){
+		for(int j = -1;j<=1;j++){
+			if(i==0 && j==0) continue;
+			
+			const char c = World_Get(world,x + j,y + i);
+			if(c == WORLD_ACTIVE) count++;
+		}
+	}
+	return count;
+}
+
+void Setup(AlxWindow* w){
+	tv = TransformedView_New((Vec2){ GetWidth(),GetHeight() });
+	TransformedView_Zoom(&tv,(Vec2){ 0.01f,0.01f });
+
+	world = Bitfield_New(WORLD_X * WORLD_Y);
+	world_target = Bitfield_New(WORLD_X * WORLD_Y);
+}
+void Update(AlxWindow* w){
+	tv.ZoomSpeed = (float)w->ElapsedTime;
+	TransformedView_HandlePanZoom(&tv,window.Strokes,(Vec2){ GetMouse().x,GetMouse().y });
+	
+	const Vec2 tl = TransformedView_ScreenWorldPos(&tv,(Vec2){ 0.0f,0.0f });
+	const Vec2 br = TransformedView_ScreenWorldPos(&tv,(Vec2){ GetWidth(),GetHeight() });
+	
+	if(Stroke(ALX_KEY_SPACE).DOWN){
+		Bitfield_Memset(&world_target,0);
+
+		for(int i = (int)F32_Floor(tl.y) - 1;i<(int)F32_Ceil(br.y);i++){
+			for(int j = (int)F32_Floor(tl.x) - 1;j<(int)F32_Ceil(br.x);j++){
+				const char c = World_Get(&world,j,i);
+				const char count = World_Get_N(&world,j,i);
+
+				if(c == WORLD_ACTIVE){
+					if(count<2 || count>3){
+						World_Set(&world_target,j,i,WORLD_INACTIVE);
+					}else{
+						World_Set(&world_target,j,i,WORLD_ACTIVE);
+					}
+				}else{
+					if(count==3){
+						World_Set(&world_target,j,i,WORLD_ACTIVE);
+					}else{
+						World_Set(&world_target,j,i,WORLD_INACTIVE);
+					}
+				}
+			}
+		}
+
+		Bitfield_Memcpy(&world,&world_target);
+	}
+
+	if(Stroke(ALX_MOUSE_L).DOWN){
+		Vec2 m = TransformedView_ScreenWorldPos(&tv,GetMouse());
+		World_Set(&world,m.x,m.y,WORLD_ACTIVE);
+	}
+	if(Stroke(ALX_MOUSE_R).DOWN){
+		Vec2 m = TransformedView_ScreenWorldPos(&tv,GetMouse());
+		World_Set(&world,m.x,m.y,WORLD_INACTIVE);
+	}
+
+	Clear(BLACK);
+	
+	for(int i = (int)F32_Floor(tl.y) - 1;i<(int)F32_Ceil(br.y);i++){
+		for(int j = (int)F32_Floor(tl.x) - 1;j<(int)F32_Ceil(br.x);j++){
+			const char c = World_Get(&world,j,i);
+			const Vec2 bg_p = TransformedView_WorldScreenPos(&tv,(Vec2){ j,i });
+			const Vec2 bg_d = TransformedView_WorldScreenLength(&tv,(Vec2){ 1.0f,1.0f });
+			
+			if(c==WORLD_ACTIVE){
+				RenderRect(bg_p.x,bg_p.y,bg_d.x + 1,bg_d.y + 1,WHITE);
+			}else if(c==WORLD_INACTIVE){
+				RenderRect(bg_p.x,bg_p.y,bg_d.x + 1,bg_d.y + 1,BLACK);
+			}else{
+				RenderRect(bg_p.x,bg_p.y,bg_d.x + 1,bg_d.y + 1,GRAY);
+			}
+		}
+	}
+}
+void Delete(AlxWindow* w){
+	Bitfield_Free(&world);
+	Bitfield_Free(&world_target);
+}
+
+int main(){
+    if(Create("Cellular Automa Bitfield",2200,1200,1,1,Setup,Update,Delete))
+        Start();
+    return 0;
+}
